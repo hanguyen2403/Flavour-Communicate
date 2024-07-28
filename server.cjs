4c0395b88075b12d39e6557b592c66a30a7e8024
Ha Nguyen <hanguyenitcs2403@gmail.com>
2024-07-28 22:13:18 +0700

    show received command

--- server.cjs
+++ server.cjs
@@ -1,18 +1,49 @@
 const express = require('express');
 const cors = require('cors');
 const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
 const { Mutex } = require('async-mutex');
-const path = require('path');
 
 const app = express();
-const PORT_NAME = 'COM9';
-const BAUD_RATE = 9600;
+const PORT_NAME = 'COM16';
+const BAUD_RATE = 115200;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const mutex = new Mutex();
+
+// Initialize the serial port once
+const port = new SerialPort({ path: PORT_NAME, baudRate: BAUD_RATE }, (err) => {
+  if (err) {
+    console.error('Failed to open port:', err.message);
+    return;
+  }
+});
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+parser.on('data', (data) => {
+  console.log(data);
+});
+
+port.on('error', (err) => {
+  console.error('Serial port error:', err.message);
+});
+
+async function writeToSerialPort(data) {
+  return new Promise((resolve, reject) => {
+    port.write(data + '\n', (err) => {
+      if (err) {
+        console.error('An error occurred while writing to the serial port.', err);
+        reject(err);
+      } else {
+        console.log('Data written to the serial port:', data.trim());
+        resolve();
+      }
+    });
+  });
+}
 
 app.post('/send-commands', (req, res) => {
   const { commands } = req.body;
@@ -30,34 +61,6 @@ app.post('/send-commands', (req, res) => {
     }
   });
 });
-
-async function writeToSerialPort(data) {
-  return new Promise((resolve, reject) => {
-    const port = new SerialPort({ path: PORT_NAME, baudRate: BAUD_RATE }, (err) => {
-      if (err) {
-        console.error('Failed to open port:', err.message);
-        reject(err);
-        return;
-      }
-
-      port.write(data + '\n', (err) => {
-        if (err) {
-          console.error('An error occurred while writing to the serial port.', err);
-          reject(err);
-        } else {
-          console.log('Data written to the serial port:', data.trim());
-          resolve();
-        }
-
-        port.close((err) => {
-          if (err) {
-            console.error('An error occurred while closing the serial port.', err);
-          }
-        });
-      });
-    });
-  });
-}
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
